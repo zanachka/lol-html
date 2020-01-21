bd3b0744b408eb05b19d0a724f7f86a67f2100e2
Ivan Nikulin <inikulin@cloudflare.com>
2020-01-21 17:30:40 +0000

    Implement JS API, part 2: rewriter (#38)

--- js-api/src/html_rewriter.rs
+++ js-api/src/html_rewriter.rs
@@ -1,8 +1,10 @@
+use super::handlers::{DocumentContentHandlers, ElementContentHandlers};
 use super::*;
 use js_sys::{Function as JsFunction, Uint8Array};
 use lol_html::{
-    DocumentContentHandlers, ElementContentHandlers, HtmlRewriter as NativeHTMLRewriter,
-    OutputSink, Selector,
+    DocumentContentHandlers as NativeDocumentContentHandlers,
+    ElementContentHandlers as NativeElementContentHandlers, HtmlRewriter as NativeHTMLRewriter,
+    OutputSink, Selector, Settings,
 };
 
 struct JsOutputSink(JsFunction);
@@ -25,7 +27,80 @@ impl OutputSink for JsOutputSink {
 }
 
 #[wasm_bindgen]
-pub struct HTMLRewriterBuilder {
-    element_content_handlers: (Selector, ElementContentHandlers<'static>),
-    document_content_handlers: DocumentContentHandlers<'static>,
+#[derive(Default)]
+pub struct HTMLRewriter {
+    selectors: Vec<Selector>,
+    element_content_handlers: Vec<NativeElementContentHandlers<'static>>,
+    document_content_handlers: Vec<NativeDocumentContentHandlers<'static>>,
+    output_sink: Option<JsOutputSink>,
+    encoding: String,
+    inner: Option<NativeHTMLRewriter<'static, JsOutputSink>>,
+}
+
+#[wasm_bindgen]
+impl HTMLRewriter {
+    #[wasm_bindgen(constructor)]
+    pub fn new(encoding: String, output_sink: &JsFunction) -> Self {
+        HTMLRewriter {
+            output_sink: Some(JsOutputSink::new(output_sink)),
+            encoding,
+            ..Self::default()
+        }
+    }
+
+    fn assert_not_fully_constructed(&self) -> JsResult<()> {
+        if self.inner.is_some() {
+            Err("Handlers can't be added after write.".into())
+        } else {
+            Ok(())
+        }
+    }
+
+    fn ensure_inner(&mut self) -> JsResult<()> {
+        if self.inner.is_none() {
+            let output_sink = self.output_sink.take().unwrap();
+            // NOTE: selector are passed by reference to the rewriter ctor, though they
+            // are not stored in the rewriter, so we need their references to be valid
+            // only during the rewriter invocation.
+            let selectors: Vec<_> = self.selectors.drain(..).collect();
+
+            let settings = Settings {
+                element_content_handlers: self
+                    .element_content_handlers
+                    .drain(..)
+                    .enumerate()
+                    .map(|(i, h)| (&selectors[i], h))
+                    .collect(),
+
+                document_content_handlers: self.document_content_handlers.drain(..).collect(),
+                encoding: &self.encoding,
+                ..Settings::default()
+            };
+
+            let rewriter = NativeHTMLRewriter::try_new(settings, output_sink).into_js_result()?;
+
+            self.inner = Some(rewriter);
+        }
+
+        Ok(())
+    }
+
+    pub fn on(&mut self, selector: &str, handlers: ElementContentHandlers) -> JsResult<()> {
+        self.assert_not_fully_constructed()?;
+
+        let selector = selector.parse::<Selector>().into_js_result()?;
+
+        self.selectors.push(selector);
+        self.element_content_handlers.push(handlers.into_native());
+
+        Ok(())
+    }
+
+    #[wasm_bindgen(method, js_name=onDocument)]
+    pub fn on_document(&mut self, handlers: DocumentContentHandlers) -> JsResult<()> {
+        self.assert_not_fully_constructed()?;
+        self.document_content_handlers.push(handlers.into_native());
+
+        Ok(())
+    }
 }
